fc138dc2e8785e121da97455c29c8756906c17b8
baugh70 <zakkyzakkba@gmail.com>
2013-11-22 10:56:09 -0500

    Update FunWithArrays.java

--- FunWithArrays.java
+++ FunWithArrays.java
@@ -1,3 +1,17 @@
 public class FunWithArrays{
+  public static FunWithArrays plugin;
+  public static ArrayList<String> fun = new ArrayList<String>();
+  
+  System.out.println(fun);
+  // Print the ArrayList
+  
+  fun.add("Keith be wallin'");
+  fun.add("Faris you nigga!");
+  
+  System.out.println(fun);
+  
+  public static ArrayList<String> computerThings = new ArrayList<String>();
+  
+  computer
 
 }
